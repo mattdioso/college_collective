1cfa01752002a0b69979373a2e1eac596261a7da
mattdioso <dioso.matt@gmail.com>
2018-03-11 17:37:06 -0700

    adding javascript to link buttons fomr school page to corresponding comment page in comment carousel

--- src/main/java/cc/web/HomeController.java
+++ src/main/java/cc/web/HomeController.java
@@ -3,6 +3,7 @@ package cc.web;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -26,5 +27,10 @@ public class HomeController {
 	@RequestMapping(value = "/schoolcomments", method = RequestMethod.GET)
 	public String goToComments() {
 		return "schoolcomments";
+	}
+
+	@RequestMapping(value="/schoolcomments?slide={slideID}", method=RequestMethod.GET)
+	public String goToSpecificComments(@PathVariable(value="slideID") String slideID) {
+		return "schoolcomments?slide=" + slideID;
 	}
 }
